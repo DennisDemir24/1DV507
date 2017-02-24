5c5c310d17ed22dbf1bc051f3a13653b3f3f78f4
reversed_word <martin.lyra.97@gmail.com>
2017-02-24 14:42:50 +0100

    Exercise 3.9

---
+++ ml224ec_assign3/catch_the_creature/CatchTheCreatureGame.java
@@ -0,0 +1,101 @@
+package ml224ec_assign3.catch_the_creature;
+
+import java.util.Random;
+
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.Region;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.ImagePattern;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.Shape;
+
+public class CatchTheCreatureGame extends Pane {
+
+	private Rectangle creature;
+	
+	private EventHandler<Event> onCaughtHandler;
+	
+	public CatchTheCreatureGame()
+	{
+		creature = new Rectangle(32,32);
+		//creature.setFill(Color.BLUE);
+		
+		Image img = new Image("/ml224ec_assign3/catch_the_creature/bug.png");
+		
+		creature.setFill(new ImagePattern(img));
+		
+		creature.setStyle("-fx-background-color: #fff");
+		
+		this.getChildren().add(creature);
+		this.setOnMouseClicked(e -> this.handleMouseClick(e));
+		
+		this.setStyle("-fx-background-color: #999");
+	}
+	
+	public void setSize(double width, double height)
+	{
+		this.setMinSize(width, height);
+		this.setPrefSize(width, height);
+		relocateCreature();
+	}
+	
+	public void setOnCreatureCaught(EventHandler<Event> handler)
+	{
+		onCaughtHandler = handler;
+	}
+	
+	private void handleMouseClick(MouseEvent e)
+	{
+		if (isWithin(e.getX(), e.getY(), creature))
+		{
+			onCaughtHandler.handle(null);
+			relocateCreature();
+		}
+	}
+	
+	private boolean isWithin(double x, double y, Rectangle rect)
+	{
+		double minX = rect.getX();
+		double maxX = minX + rect.getWidth();
+		
+		double minY = rect.getY();
+		double maxY = minY + rect.getHeight();
+		
+		return (minX <= x && x <= maxX) && (minY <= y && y <= maxY);
+	}
+	
+	private double getVaildHeight()
+	{
+		double h = this.getHeight();
+		if (h == 0)
+			h = this.getMinHeight();
+		return h;
+	}
+	
+	private double getVaildWidth()
+	{
+		double w = this.getWidth();
+		if (w == 0)
+			w = this.getMinWidth();
+		return w;
+	}
+	
+	private void relocateCreature()
+	{
+		double maxX = this.getVaildWidth() - creature.getWidth();
+		double maxY = this.getVaildHeight() - creature.getHeight();
+		
+		Random rand = new Random();
+		
+		double x = rand.nextInt((int) (maxX+1));
+		double y = rand.nextInt((int) (maxY+1));
+		
+		creature.setX(x);
+		creature.setY(y);
+	}
+}
---
+++ ml224ec_assign3/catch_the_creature/CatchTheCreatureMain.java
@@ -0,0 +1,65 @@
+package ml224ec_assign3.catch_the_creature;
+
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+
+public class CatchTheCreatureMain extends Application {
+
+	private Label countLabel;
+	
+	private int count = 0;
+	
+	public static void main(String[] args) {
+		
+		launch(args);
+
+	}
+
+	@Override
+	public void start(Stage arg0) throws Exception {
+	
+		countLabel = new Label("Times caught: 0");
+		countLabel.setMaxWidth(200);
+		countLabel.setTextFill(Color.WHITE);
+		
+		BorderPane root = new BorderPane();
+		
+		Label text = new Label("Click on the bug to catch it!");
+		text.setAlignment(Pos.CENTER);
+		
+		CatchTheCreatureGame game = new CatchTheCreatureGame();
+		
+		game.setSize(300, 250);
+		
+		game.getChildren().addAll(countLabel);
+		
+		game.setOnCreatureCaught(e -> updateCount());
+		
+		root.setTop(text);
+		root.setCenter(game);
+		
+		BorderPane.setAlignment(text, text.getAlignment());
+		
+		Scene scene = new Scene(root);
+		
+		arg0.setTitle("Catch the Bug!");
+		arg0.setScene(scene);
+		arg0.setResizable(false);
+		arg0.sizeToScene();
+		
+		arg0.show();
+	}
+
+	private void updateCount()
+	{
+		count++;
+		countLabel.setText(String.format("Times caught: %d", count));
+	}
+}
